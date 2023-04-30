df52943954203c31396070bb1e0f4a8a2651c84d
Aryan <aryan4p130202@gmail.com>
2023-04-30 15:03:36 +0530

    feature: names detected along with text in ui with multiple connections

--- models/Chat.js
+++ models/Chat.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
-    chat: {
-        type: String,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  chat: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+  },
 });
 
-const Chat = mongoose.model('Chat', chatSchema);
+const Chat = mongoose.model("Chat", chatSchema);
 module.exports = Chat;
